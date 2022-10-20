adc66d2f87893530e50bac16694e2938781bd46e
Ade Rachmadi <ade.rachmadi@iconpln.co.id>
2022-10-20 16:29:20 +0700

    team eight

---
+++ script.js
@@ -0,0 +1,19 @@
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+
+      window.location.href = "thankyou.html";
+    })
+    .catch((e) => alert("No Signal"));
+});
